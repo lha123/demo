ed4788f2c43f6669bb55164ee3ab418339643853
liuhonger <l1277213149@163.com>
2023-06-05 15:53:22 +0800

    恢复规划

---
+++ src/main/java/com/example/demo/redisson/Address.java
@@ -0,0 +1,14 @@
+package com.example.demo.redisson;
+
+
+import lombok.Data;
+
+@Data
+public class Address {
+
+    private String province;
+
+    private String distinct;
+
+    private String county;
+}
--- src/main/java/com/example/demo/redisson/RedissonConfigYml.java
+++ src/main/java/com/example/demo/redisson/RedissonConfigYml.java
@@ -1,13 +1,43 @@
 package com.example.demo.redisson;
 
 
-//@Component
-//@Data
-//@ConfigurationProperties(prefix = "config")
-//@PropertySource(value = "classpath:redisson.yml",encoding = "UTF-8")  //读取指定路径配置文件
-//public class RedissonConfigYml {
-//
-//    private SentinelServersConfig sentinelServersConfig;
-//
-//
-//}
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "person")
+@PropertySource(value = "classpath:person.yml",factory = YamlPropertySourceFactory.class,encoding = "UTF-8")  //读取指定路径配置文件
+public class RedissonConfigYml {
+
+    private String id;
+
+    private String name;
+
+    private int age;
+
+    private boolean isManager;
+
+    private Date birthday;
+
+    private Map<String, Object> map;
+
+    private List<String> list;
+
+    private Address address;
+
+
+    @PostConstruct
+    public void show(){
+        System.out.println(this);
+    }
+
+
+}
